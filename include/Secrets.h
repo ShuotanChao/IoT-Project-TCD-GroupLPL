5a0feec0b40a0b94ed81d5128746959ce9447ae6
Sherlock Wan <tingzhou@ualberta.ca>
2023-04-02 16:22:35 +0100

    changes in reading air quality and sound level

--- include/Secrets.h
+++ include/Secrets.h
@@ -3,7 +3,7 @@
 #define SECRET
 #define THINGNAME "CRCdeESP32" // change this
 
-const char WIFI_SSID[] = "Shuotan Chao's iPhone";                                           // change this
+const char WIFI_SSID[] = "ChaoiPhone";                                           // change this
 const char WIFI_PASSWORD[] = "609940890";                                        // change this
 const char AWS_IOT_ENDPOINT[] = "a1l6oi0gih8xsu-ats.iot.eu-west-1.amazonaws.com"; // change this
 
